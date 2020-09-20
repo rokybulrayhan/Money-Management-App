09a09bac969699d6e7050137a185fba0bdb72c8e
Rokybul Rayhan Chowdhury <rokybulrayhanchowdhury@gmail.com>
2020-09-20 23:25:57 +0600

    nodejs react project setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const passport = require('passport')
+//var passport = require("passport");
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+
+    res.json({
+        message: 'Welcome To Our Application'
+    })
+})
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    console.log(`SERVER is RUNNING ON PORT ${PORT}`)
+    
+})
